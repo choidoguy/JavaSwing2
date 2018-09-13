39b1a0a8065e4d2e26b73637b0f0d4c020f644f6
choidoguy <chlehrb@hanmail.net>
2018-09-13 14:48:56 +0900

    file drop

--- JavaSwing/src/step/STEP01.java
+++ JavaSwing/src/step/STEP01.java
@@ -1,14 +1,17 @@
 package step;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.util.Vector;
 
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
 
-import come.CenPanel;
-import come.EasPanel;
 import come.NorPanel;
 import come.SouPanel;
-import come.WesPanel;
+import file.FileDrop;
 import main.Scheduler;
 
 @SuppressWarnings("serial")
@@ -16,7 +19,6 @@ public class STEP01 extends JPanel {
     
     public STEP01(Scheduler scheduler, String sPrev, String sNext) {
     	BorderLayout borderLayout = new BorderLayout();
-    	setLayout(borderLayout);
     	
     	NorPanel norPanel = new NorPanel("STEP01","다중파일선택");
     	add(norPanel);
@@ -33,10 +35,37 @@ public class STEP01 extends JPanel {
     	SouPanel souPanel = new SouPanel(scheduler, sPrev, sNext);
     	add(souPanel);  
     	borderLayout.addLayoutComponent(souPanel, BorderLayout.SOUTH);
+
+    	JPanel cenPanel = new JPanel();
+    	cenPanel.setBorder(new TitledBorder(new LineBorder(Color.black),""));
     	
-    	CenPanel cenPanel = new CenPanel();
+    	
+		JList list = new JList();
+		cenPanel.add(list);
+		
+		new FileDrop(System.out, list, /* dragBorder, */ new FileDrop.Listener() {
+			public void filesDropped(java.io.File[] files) {
+				Vector vec = new Vector();
+				for (int i = 0; i < files.length; i++) {
+					try {
+						vec.addElement(files[i].getCanonicalPath() + "\n");
+					} // end try
+					catch (java.io.IOException e) {
+					}
+				} // end for: through each dropped file
+				list.setListData(vec);
+			} // end filesDropped
+		}); // end FileDrop.Listener
+		
+		BorderLayout layout = new BorderLayout();
+		layout.addLayoutComponent(list, BorderLayout.CENTER);
+		
+		cenPanel.setLayout(layout);
+		
+    	
     	add(cenPanel); 
     	borderLayout.addLayoutComponent(cenPanel, BorderLayout.CENTER);
+    	setLayout(borderLayout);
 		
     } // end public STEP01(Scheduler scheduler)
     
